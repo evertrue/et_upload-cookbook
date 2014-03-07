e50a031bc5ac5a0ee951135602e2fc49eb71a6af
Jeff Byrnes <jeff@evertrue.com>
2014-03-07 12:24:07 -0500

    Ensure OpenSSH recipe is run

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,6 +24,7 @@
 # 6. Copy necessary system files to chroot jail (users will not read things outside jail)
 # 7. Set up users
 
+include_recipe 'openssh::default'
 include_recipe 'et_upload::scripts'
 include_recipe 'et_upload::chroot_jail'
 include_recipe 'et_upload::users'

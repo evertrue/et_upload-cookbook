14844ffc1fa863b7a3a0e7db9b15973983da71b2
Jeff Byrnes <jeff@evertrue.com>
2014-03-17 17:53:17 -0400

    Add ChefSpec tests for et_upload::users

--- spec/recipes/users_spec.rb
+++ spec/recipes/users_spec.rb
@@ -3,11 +3,73 @@ require 'spec_helper'
 describe 'et_upload::default' do
   let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
 
+  users = {
+    'upload' => {
+      'id' => 'upload',
+      'penncharter4616' => {
+        'uid'      => 10_042,
+        'ssh_keys'     => 'ssh-key-1',
+        'comment'  => 'Test User 1',
+        'password' => 'password'
+      },
+      'randolphschool6139' => {
+        'uid'      => 10_041,
+        'ssh_keys'     => 'ssh-key-2',
+        'comment'  => 'Test User 2',
+        'password' => 'password'
+      }
+    }
+  }
+
   before do
     stub_command('test -d /opt/evertrue/upload').and_return(0)
+
+    ChefSpec::Server.create_data_bag('users', users)
   end
 
-  # it 'includes openssh::default' do
-  #   expect(chef_run).to include_recipe('openssh::default')
-  # end
+  users['upload'].each do |uname, u|
+    if uname != 'id'
+      u['home'] = "/home/#{uname}"
+      u['gid']  = 'uploadonly'
+
+      it "creates user #{uname}" do
+        expect(chef_run).to create_user(uname).with(
+          uid:      u['uid'],
+          gid:      u['gid'],
+          comment:  u['comment'],
+          password: u['password'],
+          home:     u['home']
+        )
+      end
+
+      it "sets #{uname} home folder mode and ownership" do
+        expect(chef_run).to create_directory(u['home']).with(
+          user:  'root',
+          group: u['gid'],
+          mode:  '0755'
+        )
+      end
+
+      ["#{u['home']}/.ssh", "#{u['home']}/uploads"].each do |dir|
+        it "creates #{dir}" do
+          expect(chef_run).to create_directory(dir).with(
+            user:  uname,
+            group: u['gid'],
+            mode:  '0700'
+          )
+        end
+      end
+
+      it "creates #{uname}'s authorized_keys" do
+        auth_keys_path = "#{u['home']}/.ssh/authorized_keys"
+
+        expect(chef_run).to create_template(auth_keys_path).with(
+          user:  uname,
+          group: u['gid'],
+          mode:  '0600'
+        )
+        expect(chef_run).to render_file(auth_keys_path).with_content(u['keys'])
+      end
+    end
+  end
 end

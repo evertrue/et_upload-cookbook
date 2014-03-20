38528491d199c20a5dffc29559a55ace88d13a47
Hai Zhou <islandsword@gmail.com>
2014-03-20 11:00:18 -0400

    Add ehren on notification

--- recipes/scripts.rb
+++ recipes/scripts.rb
@@ -37,7 +37,7 @@ end
 
 shell  = '/bin/bash'
 path   = '/sbin:/bin:/usr/sbin:/usr/bin'
-mailto = 'hai.zhou+upload@evertrue.com'
+mailto = 'ehren+upload@evertrue.com,hai.zhou+upload@evertrue.com'
 
 cron_d 'show_uploads' do
   minute  0

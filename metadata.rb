f889f0a09bd1fb300311ba99c567d103da6811d5
Eric Herot <eric.github@herot.com>
2015-04-13 15:08:08 -0400

    Use greater-than cron version constraint

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures et_upload'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '2.0.14'
 
-depends 'cron',         '= 1.3.8'
+depends 'cron',         '>= 1.3.8'
 depends 'yum',          '~> 3.1'
 depends 'apt',          '~> 2.3'
 depends 'openssh',      '~> 1.3'

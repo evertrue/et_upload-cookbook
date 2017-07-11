6282163a9bfa109878cddab6a07af394cd075394
Ayush Sangani <ayushsangani@gmail.com>
2017-07-11 13:48:03 -0400

    Bump to v5.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jeff@evertrue.com'
 license          'Apache 2.0'
 description      'Installs/Configures et_upload'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '4.0.0'
+version          '5.0.0'
 
 depends 'cron',         '>= 1.3.8'
 depends 'yum',          '~> 3.1'

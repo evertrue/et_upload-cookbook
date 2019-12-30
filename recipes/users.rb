f789271f7804e3e0aba91bf6867c0b7872915ed6
grosezoom <george@evertrue.com>
2019-12-30 08:35:23 -0500

    ET-10897: Add support for partner user upload processing

--- recipes/users.rb
+++ recipes/users.rb
@@ -29,7 +29,13 @@ upload_users = data_bag_item('users', 'upload').select { |uname| uname != 'id' }
 
 evertrue_gid = 'evertrue'
 upload_users.each do |uname, u|
-  u['home'] = "#{node['et_upload']['base_dir']}/users/#{uname}"
+  # If a parent_user and partner have been provided then the user's home folder is the partner directory under the parent_user's home folder
+  if u['parent_user'] && u['partner']
+    u['home'] = "#{node['et_upload']['base_dir']}/users/#{u['parent_user']}/#{u['partner']}"
+  else
+    u['home'] = "#{node['et_upload']['base_dir']}/users/#{uname}"
+  end
+
   u['gid'] = 'uploadonly'
 
   if u['action'] == 'remove'
